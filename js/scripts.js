caa161b8691dc4a8246bb1391060440609032ef0
Alexandra Holcombe and sandro mijatovic <sandro@>
2017-01-19 10:23:42 -0800

    add function that compares userInput to arabicEquivalents[] then outputs correlating index of romanNumerals[] add branch that loops through input if <= 3 and concats an I to result variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
-
+var userInput = 0
 var romanNumerals = ["I", "V", "X", "L", "C", "D", "M"];
 var arabicEquivalents = [1, 5, 10, 50, 100, 500, 1000];
+var firstThree;
+var result = "I";
 
-$(function {
+var settingBase = function(input) {
+  result = "I"
+  for(var index = 0; index < arabicEquivalents.length; index++) {
+    if(input === arabicEquivalents[index]) {
+      return romanNumerals[index];
+    } else if (input <= 3) {
+      for (var i = 0; i <= input; i++) {
+        result += "I";
+        return result;
+      };
+    };
+  };
+};
+
+
+
+$(function() {
   $("form").submit(function() {
     event.preventDefault();
+    userInput = parseInt($("#user-input").val());
 
-    $("#result").text();
+    $("#result").text(settingBase(userInput));
   });
-}):
+});
